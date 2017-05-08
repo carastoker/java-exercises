7cab540bdb76ca9272c4198b8378d164ae0e193d
cstoker <stoker.cara@gmail.com>
2017-05-08 12:04:47 -0500

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by cstoker on 5/8/17.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("Hello2");
+    }
+
+}
