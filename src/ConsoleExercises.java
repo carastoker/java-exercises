3950d363f7fe6c9c91d71729fcc941fbd331d0dc
cstoker <stoker.cara@gmail.com>
2017-05-08 17:24:10 -0500

    completed java syntax, data types, and variables exercise.

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,53 @@
+/**
+ * Created by cstoker on 5/8/17.
+ */
+
+import java.util.Scanner;
+
+public class ConsoleExercises {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+//        System.out.println("Enter your age");
+//        int age = scan.nextInt();
+//        System.out.println(age);
+//
+//        System.out.println("Enter your name");
+//        String name = scan.nextLine();
+//        System.out.println(name);
+//
+//        System.out.println("Enter your favorite language");
+//        String lang = scan.nextLine();
+//        System.out.println(lang);
+//
+//        System.out.println("Can you whistle");
+//        String whistle = scan.nextLine();
+//        System.out.println(whistle);
+//
+//        System.out.println("Do you prefer italian or indian food");
+//        String food = scan.nextLine();
+//        System.out.println(food);
+
+        float height;
+        float width;
+        float length;
+
+
+        System.out.println("Enter the height of your classroom");
+        height = scan.nextFloat();
+        System.out.println("Enter the width of your classroom");
+        width = scan.nextFloat();
+        System.out.println("Enter the length of your classroom");
+        length = scan.nextFloat();
+
+//      Java printf( ) Method Quick Reference
+        System.out.println("Here are the room calculations");
+        System.out.printf("area= %.2f sqft\n", length*width);
+        System.out.printf("perimeter= %.2fft %.2fft\n", (2 * width),(2 * length));
+        System.out.printf("volume= %.2f cu/ft\n", (length * height * width));
+
+    }
+
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,46 @@ public class HelloWorld {
 
         System.out.println("Hello");
         System.out.println("Hello2");
+
+//    -----------------------------------------
+
+        int myFavoriteNumber = 27;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "3.14159";
+        System.out.println(myString);
+
+       double myNumber = 3.14;
+        System.out.println(myNumber);
+
+//    ------------------------------------------
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+//    ------------------------------------------
+
+//        int x = 4;
+//        x = x + 5;
+//
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+
     }
 
+
+
 }
+
+
