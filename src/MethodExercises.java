b89cd886d91a5cba05f4ba61ebc6d5a6c866c3bc
cstoker <stoker.cara@gmail.com>
2017-05-11 16:48:16 -0500

    created methods and tested to confirm the output.

---
+++ src/MethodExercises.java
@@ -0,0 +1,30 @@
+/**
+ * Created by cstoker on 5/11/17.
+ */
+public class MethodExercises {
+
+    public static int addition(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    //
+    public static int subtraction(int x, int y) {
+        return x - y;
+    }
+//
+    public static int multiplication(int c, int s) {
+        return c * s;
+    }
+//
+    public static int division(int n, int m) {
+        return n / m;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(division(1, 2));
+    }
+
+//        Scanner scan = new Scanner(System.in);
+
+}
