77c695de5b96ca138081ec108b5ddaec9d374602
cstoker <stoker.cara@gmail.com>
2017-05-16 17:25:43 -0500

    tested the understanding references exercised.

--- src/Java2/PersonApp.java
+++ src/Java2/PersonApp.java
@@ -10,10 +10,29 @@ public class PersonApp {
         Person personUno = new Person("Jubal");
         Person personDos = new Person("Valentine");
 
-        personUno.sayHello();
-        personDos.sayGoodbye();
+//        personUno.sayHello();
+//        personDos.sayGoodbye();
+
+//        System.out.println(personUno.getName().equals(personDos.getName()));
+//        System.out.println(personUno == personDos);
 
 
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+//
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jaime");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
 
     }
 }
