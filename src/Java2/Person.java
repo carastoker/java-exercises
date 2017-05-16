50fce8e182f9806375370e8906aef18e3bb4f99b
cstoker <stoker.cara@gmail.com>
2017-05-16 16:58:27 -0500

    added the following methods: person, getName, setName, sayHello and sayGoodby. created class for a main method.

--- src/Java2/Person.java
+++ src/Java2/Person.java
@@ -5,11 +5,33 @@ package Java2;
  */
 public class Person {
 
-//        Person name = new Person();
-
     private String name;
 
 
+//  constructor
+    public Person(String personName) {
+
+        this.name = personName;
+    }
+
+//  getter
+    public String getName() {
+        return name;
+    }
+
+//  setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sayHello() {
+
+        System.out.println("Hello, " + name);
+    }
+
+    public void sayGoodbye() {
+        System.out.println("Seeya later, " + name + "!");
+    }
 
 
 }
---
+++ src/Java2/PersonApp.java
@@ -0,0 +1,19 @@
+package Java2;
+
+/**
+ * Created by cstoker on 5/16/17.
+ */
+public class PersonApp {
+
+    public static void main(String[] args) {
+
+        Person personUno = new Person("Jubal");
+        Person personDos = new Person("Valentine");
+
+        personUno.sayHello();
+        personDos.sayGoodbye();
+
+
+
+    }
+}
