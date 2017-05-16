3da9c4d750f9a87115466bc242dc5c2de1c7ab09
cstoker <stoker.cara@gmail.com>
2017-05-16 17:26:48 -0500

    removed understanding references exercised.

--- src/Java2/PersonApp.java
+++ src/Java2/PersonApp.java
@@ -13,26 +13,8 @@ public class PersonApp {
 //        personUno.sayHello();
 //        personDos.sayGoodbye();
 
-//        System.out.println(personUno.getName().equals(personDos.getName()));
-//        System.out.println(personUno == personDos);
-
-
-//        Person person1 = new Person("John");
-//        Person person2 = new Person("John");
-//        System.out.println(person1.getName().equals(person2.getName()));
-//        System.out.println(person1 == person2);
-//
-//        Person person1 = new Person("John");
-//        Person person2 = person1;
-//        System.out.println(person1 == person2);
-
-        Person person1 = new Person("John");
-        Person person2 = person1;
-        System.out.println(person1.getName());
-        System.out.println(person2.getName());
-        person2.setName("Jaime");
-        System.out.println(person1.getName());
-        System.out.println(person2.getName());
+        System.out.println(personUno.getName().equals(personDos.getName()));
+        System.out.println(personUno == personDos);
 
     }
 }
