0e8b56dfb71a132d6e3f71e2239a13c5fbdc5952
cstoker <stoker.cara@gmail.com>
2017-05-23 10:16:01 -0500

    displays the list of students, and prompts you to select a students name. when you choose a student it returns the students name and grade average grade from hashmap, if input is not invalid it returns you to student list. prompts you to continue selecting students with yesNo method.

--- src/Java2/StudentApp.java
+++ src/Java2/StudentApp.java
@@ -1,5 +1,8 @@
 package Java2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by cstoker on 5/22/17.
  */
@@ -7,13 +10,60 @@ public class StudentApp {
 
     public static void main(String[] args) {
 
-        Student one = new Student("Bart");
-        one.addGrade(93);
-        one.addGrade(86);
-        one.addGrade(78);
-        one.addGrade(90);
+        Input otherInput = new Input();
+        boolean validInput;
 
-        System.out.println(one.getGradeAverage());
+        Map<String, Student> students = new HashMap();
+
+        Student Bart = new Student("Bart");
+        Bart.addGrade(93);
+        Bart.addGrade(86);
+        Bart.addGrade(78);
+        Bart.addGrade(90);
+        students.put("git", Bart);
+
+        Student Homer = new Student("Homer");
+        Homer.addGrade(72);
+        Homer.addGrade(75);
+        Homer.addGrade(70);
+        Homer.addGrade(69);
+        students.put("git", Homer);
+
+        Student Lisa = new Student("Lisa");
+        Lisa.addGrade(99);
+        Lisa.addGrade(95);
+        Lisa.addGrade(96);
+        Lisa.addGrade(98);
+        do {
+
+            System.out.println("Welcome!\n\n" + "Here are the github usernames of our students:\n\n" +
+                    "|" + Bart.getName() + "|" + Lisa.getName() + "|" + Homer.getName() + "|\n\n");
+
+            do {
+
+                validInput = true;
+
+                String userInput = otherInput.getString("What student would you like to see more information on?");
+
+                if (userInput.equalsIgnoreCase(Bart.getName())) {
+                    System.out.println("Student: " + Bart.getName() + " Grade Average: " + Bart.getGradeAverage());
+                } else if (userInput.equalsIgnoreCase(Homer.getName())) {
+                    System.out.println("Student: " + Homer.getName() + " Grade Average: " + Homer.getGradeAverage());
+                } else if (userInput.equalsIgnoreCase(Lisa.getName())) {
+                    System.out.println("Student: " + Lisa.getName() + " Grade Average: " + Lisa.getGradeAverage());
+                } else {
+                    validInput = false;
+                    System.out.println("Nope, that's not a student.");
+                }
+
+            } while (!validInput);
+
+
+        }while(otherInput.yesNo("\n Would you like to continue? Y/N"));
+
+//        System.out.println("Student: " +  Bart.getName() + " Grade Average: " + Bart.getGradeAverage());
+//        System.out.println("Student: " + Homer.getName() + " Grade Average: " + Homer.getGradeAverage());
+//        System.out.println("Student: " + Lisa.getName() + " Grade Average: " + Lisa.getGradeAverage());
 
     }
 }
